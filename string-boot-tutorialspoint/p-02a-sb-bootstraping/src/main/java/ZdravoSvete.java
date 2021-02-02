2b21aa62b778a2df313e10ff49f87dc2c605fdec
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-02-02 21:33:46 +0100

    dodao tutorialspoint projekte

---
+++ string-boot-tutorialspoint/p-02a-sb-bootstraping/src/main/java/ZdravoSvete.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class ZdravoSvete {
+
+    @RequestMapping("/")
+    String home() {
+        return "Zdravo svete!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdravoSvete.class, args);
+    }
+
+}
