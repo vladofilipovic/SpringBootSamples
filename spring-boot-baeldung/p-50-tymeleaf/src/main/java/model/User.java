159c0043f8a5eac18aa431589b897466eb03c7cc
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-02-06 21:51:24 +0100

    popravio

---
+++ spring-boot-baeldung/p-50-tymeleaf/src/main/java/model/User.java
@@ -0,0 +1,15 @@
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}
